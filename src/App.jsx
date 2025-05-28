d9741665f3b9b5966fe1932493606261ea6c881a
prasathvjkmr <prasathvjkmr@gmail.com>
2025-05-28 17:49:25 +0530

    Added axios dependency and implement user fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function getUsers() {
+      await axios
+        .get("https://reqres.in/api/users", {
+          headers: {
+            "x-api-key": "reqres-free-v1",
+          },
+        })
+        .then((res) => setUsers(res.data.data))
+        .catch((err) => console.error(err));
+    }
+    getUsers();
+  }, []);
   return (
     <>
       <h1>Claue</h1>
+      {users && users.map((user) => <p key={user.id}>{user.first_name}</p>)}
     </>
   );
 }
